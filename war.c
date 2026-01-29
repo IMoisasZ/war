e030683519a49a2fba6b474d1a2d4174ccf3c7da
moisas <mopri08@gmail.com>
2026-01-29 00:08:29 -0300

    Criado a logica do nivel aventureiro

--- war.c
+++ war.c
@@ -1,146 +1,177 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> // Para malloc/calloc e rand
-#include <time.h>   // Para inicializar a semente do rand
+#include <stdlib.h>
+#include <time.h>
 
-// Estrutura solicitada
-struct Territorio
+typedef struct
 {
     char nome[30];
     char cor[10];
     int tropas;
-};
+} Territorio;
 
-// Protótipos das funções
 void limparBuffer();
-void cadastrarTerritorio(struct Territorio *t, int i);
-void exibirMapa(struct Territorio *mapa, int total);
-void atacar(struct Territorio *atacante, struct Territorio *defensor);
-void liberarMemoria(struct Territorio *mapa);
+void cadastrarTerritorio(Territorio *t, int i);
+void exibirMapa(Territorio *mapa, int total);
+// Adicionamos o parâmetro minTropas para a função conhecer a regra escolhida
+void atacar(Territorio *atacante, Territorio *defensor, int minTropas);
+void liberarMemoria(Territorio *mapa);
 
 int main()
 {
-    struct Territorio *war = NULL;
-    int numTerritorios;
+    Territorio *war = NULL;
+    int numTerritorios = 0;
+    int minTropasAtaque = 0; // Variável que substituirá a constante
+    int idxAtk, idxDef;
 
-    // Semente para aleatoriedade
     srand(time(NULL));
 
-    printf("Quantos territorios deseja cadastrar? ");
+    printf("=====================================\n");
+    printf("   WAR ESTRUTURADO - CADADSTRO   \n");
+    printf("=====================================\n");
+
+    // Configuração de Territórios
+    printf("Informe o numero total de territorios: ");
     scanf("%d", &numTerritorios);
     limparBuffer();
 
-    // Alocação dinâmica conforme solicitado
-    war = (struct Territorio *)calloc(numTerritorios, sizeof(struct Territorio));
+    if (numTerritorios < 2)
+    {
+        printf("Minimo de 2 territorios para jogar.\n");
+        return 1;
+    }
 
+    // Configuração de Regras
+    printf("\n--- REGRAS DE COMBATE ---\n");
+    printf("No War real, um territorio precisa de 2 tropas para atacar\n");
+    printf("(1 fica protegendo a base e a outra sai para a batalha).\n");
+    printf("Quantas tropas minimas voce deseja para permitir um ataque? ");
+    scanf("%d", &minTropasAtaque);
+    limparBuffer();
+
+    war = (Territorio *)calloc(numTerritorios, sizeof(Territorio));
     if (war == NULL)
     {
         printf("Erro ao alocar memoria.\n");
         return 1;
     }
 
-    // Cadastro inicial
     for (int i = 0; i < numTerritorios; i++)
     {
         cadastrarTerritorio(&war[i], i);
     }
 
-    // Exemplo de fluxo de ataque
-    exibirMapa(war, numTerritorios);
-
-    if (numTerritorios >= 2)
+    int continuar = 1;
+    while (continuar)
     {
-        int idxAtk, idxDef;
-        printf("\n--- INICIAR ATAQUE ---\n");
-        printf("Indice do atacante (0 a %d): ", numTerritorios - 1);
+        exibirMapa(war, numTerritorios);
+
+        printf("\n--- FASE DE ATAQUE (Minimo para atacar: %d) ---\n", minTropasAtaque);
+        printf("Escolha o territorio atacante (1 a %d, ou 0 para sair): ", numTerritorios);
         scanf("%d", &idxAtk);
-        printf("Indice do defensor (0 a %d): ", numTerritorios - 1);
+
+        if (idxAtk == 0)
+        {
+            continuar = 0;
+            break;
+        }
+
+        printf("Escolha o territorio defensor (1 a %d): ", numTerritorios);
         scanf("%d", &idxDef);
 
-        // Validação: Não pode atacar a própria cor
-        if (strcmp(war[idxAtk].cor, war[idxDef].cor) == 0)
+        if (idxAtk < 1 || idxAtk > numTerritorios || idxDef < 1 || idxDef > numTerritorios)
         {
-            printf("\nERRO: Voce nao pode atacar um territorio da mesma cor (%s)!\n", war[idxAtk].cor);
+            printf("Indice invalido!\n");
+        }
+        else if (idxAtk == idxDef)
+        {
+            printf("Um territorio nao pode atacar a si mesmo!\n");
+        }
+        else if (strcmp(war[idxAtk - 1].cor, war[idxDef - 1].cor) == 0)
+        {
+            printf("Voce nao pode atacar sua propria cor!\n");
         }
         else
         {
-            atacar(&war[idxAtk], &war[idxDef]);
+            // Passamos a regra escolhida para a função
+            atacar(&war[idxAtk - 1], &war[idxDef - 1], minTropasAtaque);
         }
     }
 
-    exibirMapa(war, numTerritorios);
-
-    // Finalização
     liberarMemoria(war);
     return 0;
 }
 
-// Implementação da função de ataque com lógica de conquista
-void atacar(struct Territorio *atacante, struct Territorio *defensor)
+void atacar(Territorio *atacante, Territorio *defensor, int minTropas)
 {
-    if (atacante->tropas <= 1)
+    // Validação baseada na escolha do usuário
+    if (atacante->tropas < minTropas)
     {
-        printf("O atacante %s nao tem tropas suficientes (minimo 2).\n", atacante->nome);
+        printf("\n[AVISO] %s tem %d tropas. O minimo para atacar e' %d.\n",
+               atacante->nome, atacante->tropas, minTropas);
         return;
     }
 
     int dadoAtk = (rand() % 6) + 1;
     int dadoDef = (rand() % 6) + 1;
 
-    printf("\nBatalha: %s [%d] vs %s [%d]\n", atacante->nome, dadoAtk, defensor->nome, dadoDef);
+    printf("\n--- BATALHA: %s (%d) vs %s (%d) ---\n",
+           atacante->nome, dadoAtk, defensor->nome, dadoDef);
 
     if (dadoAtk > dadoDef)
     {
-        // Atacante vence a rodada
         defensor->tropas--;
-        printf("Vitoria no ataque! %s perdeu 1 tropa.\n", defensor->nome);
+        printf("Vitoria do ataque! %s perdeu 1 tropa.\n", defensor->nome);
 
-        // Regra de Conquista: Se as tropas do defensor acabarem
         if (defensor->tropas <= 0)
         {
-            printf("TERRITORIO CONQUISTADO! %s agora pertence ao exercito %s.\n", defensor->nome, atacante->cor);
-            strcpy(defensor->cor, atacante->cor);    // Transfere a cor
-            defensor->tropas = atacante->tropas / 2; // Transfere metade das tropas
-            atacante->tropas -= defensor->tropas;    // Remove do atacante
+            printf("CONQUISTA! %s agora pertence aos %s.\n", defensor->nome, atacante->cor);
+            strcpy(defensor->cor, atacante->cor);
+            defensor->tropas = atacante->tropas / 2;
+            atacante->tropas -= defensor->tropas;
         }
     }
     else
     {
-        // Defensor vence ou empate
         atacante->tropas--;
-        printf("O defensor resistiu! %s perdeu 1 tropa.\n", atacante->nome);
+        printf("O defensor venceu! %s perdeu 1 tropa.\n", atacante->nome);
     }
 }
 
-// Função para liberar a memória alocada
-void liberarMemoria(struct Territorio *mapa)
-{
-    if (mapa != NULL)
-    {
-        free(mapa);
-        printf("\nMemoria liberada com sucesso.\n");
-    }
-}
+// ... (Funções cadastrarTerritorio, exibirMapa, liberarMemoria e limparBuffer permanecem iguais)
 
-void cadastrarTerritorio(struct Territorio *t, int i)
+void cadastrarTerritorio(Territorio *t, int i)
 {
-    printf("\nTerritorio %d - Nome: ", i);
-    scanf(" %29[^\n]", t->nome);
+    printf("\n--- Cadastrando Territorio %d ---\n", i + 1);
+    printf("Nome do Territorio: ");
+    scanf("%s", t->nome);
     limparBuffer();
-    printf("Cor: ");
-    scanf(" %9s", t->cor);
+    printf("Cor do Territorio: ");
+    scanf("%s", t->cor);
     limparBuffer();
-    printf("Tropas: ");
+    printf("Numero de Tropas: ");
     scanf("%d", &t->tropas);
     limparBuffer();
 }
 
-void exibirMapa(struct Territorio *mapa, int total)
+void exibirMapa(Territorio *mapa, int total)
 {
-    printf("\n--- MAPA ATUAL ---\n");
+    printf("\n=====================================\n");
+    printf("          MAPA - ESTADO ATUAL          \n");
+    printf("=====================================\n");
     for (int i = 0; i < total; i++)
     {
-        printf("[%d] %s | Cor: %s | Tropas: %d\n", i, mapa[i].nome, mapa[i].cor, mapa[i].tropas);
+        printf("%d. %s | Exercito: %s | Tropas: %d\n",
+               i + 1, mapa[i].nome, mapa[i].cor, mapa[i].tropas);
+    }
+}
+
+void liberarMemoria(Territorio *mapa)
+{
+    if (mapa != NULL)
+    {
+        free(mapa);
+        printf("\nMemoria liberada. Ate a proxima!\n");
     }
 }
 
